cd977e0d9c67e874d3edd788419126b1565372ea
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-08-22 13:52:29 -0300

    se arreglan problemas de ruta

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ var minifyCSS = require ('gulp-minify-css');
 
 // 1era tarea: "Script" concatena archivos js
 gulp.task('script', function(){
-	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/materialize-css/dist/js/materialize.js', 'assets/js/*.js'])
+	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/materialize-css/dist/js/materialize.js', 'public/assets/js/main.js'])
 	.pipe(concat('script.js'))//guardados en script.js
 	.pipe(gulp.dest('public/assets/js'));//en la carpeta "dist"
 });
 
 // 2da tarea: "Style" concatena y minifica main.css
 gulp.task('style', function(){
-	gulp.src(['node_modules/materialize-css/dist/css/materialize.css', 'assets/sass/main.scss'])
+	gulp.src(['node_modules/materialize-css/dist/css/materialize.css', 'public/assets/sass/main.scss'])
 	.pipe(sass().on('error', sass.logError))
 	.pipe(minifyCSS())
 	.pipe(concat('style.min.css'))//lo convierte en style.min.css
