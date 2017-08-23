7020e6be316d94f3ddb2547984930337769f4dbe
Valentina Miranda <valentina.mirandaj@gmail.com>
2017-08-23 13:00:51 -0300

    pantalla cero y tres

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,24 @@
+/* INICIO PANTALLA TRES*/
+function c(){
+	var n=$('.c').attr('id');
+    var c=n;
+	$('.c').text(c);
+	setInterval(function(){
+		c--;
+		if(c>=0){
+			$('.c').text(c);
+		}
+        if(c==0){
+            $('.c').text(n);
+        }
+	},1000);
+}
 
+// Start
+c();
+
+// Loop
+setInterval(function(){
+	c();
+},21000);
+/* FIN PANTALLA TRES*/
