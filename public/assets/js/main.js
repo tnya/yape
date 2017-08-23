bb6c3125983c53b71011f4b7ac094045b05c913f
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-08-23 17:10:39 -0300

    se agrega la funcion para validar

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -22,3 +22,34 @@ setInterval(function(){
 	c();
 },21000);
 /* FIN PANTALLA TRES*/
+
+console.log(localStorage.getItem(code))
+
+required = function(fields) {
+    var valid = true;
+    fields.each(function () { // iterate all
+        var $this = $(this);
+        if (($this.is(':checkbox') && !$this.is(":checked")) || // checkbox
+        (($this.is(':text')) && !$this.val()) || (($this.is(':password')) && !$this.val())) {
+            valid = false;
+        }
+    });
+    return valid;
+}
+
+validateRealTimeTres = function () {
+    var fields = $("form :input:not(:hidden)"); // select required
+    fields.on('keyup change keypress blur', function () {
+        var codigo = $("#phone").val();
+        var codigoValidate = (/^[0-9]{9}$/).test(codigo);
+        console.log(codigo)
+        if (required(fields) && codigoValidate) {
+            {console.log("holi")} // action if all valid
+
+        } else {
+            {$(".btn-continuar").addClass("disabled")} // action if not valid
+        }
+    });
+}
+
+validateRealTimeTres();
